9d7f276a44927358a96badebe9e77dd28cd3b7fe
level2man <143778622+level2man@users.noreply.github.com>
2023-10-14 23:21:33 -0400

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -26652,10 +26652,10 @@ export const Moves: {[moveid: string]: MoveData} = {
 			if (this.randomChance(70, 100)) {
 				self: {
 					status: 'brn',
-				},
-			},
+				}
+			}
 			
-		},
+		}
 		secondary: null
 		target: "normal",
 		type: "Umbral",
