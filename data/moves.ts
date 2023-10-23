a4ea2e2189cfe938b975937920c9bbcd12041125
level2man <143778622+level2man@users.noreply.github.com>
2023-10-22 23:48:02 -0400

    Add files via upload

--- data/moves.ts
+++ data/moves.ts
@@ -25012,13 +25012,6 @@ export const Moves: {[moveid: string]: MoveData} = {
 		pp: 20,
 		priority: 0,
 		flags: {contact: 1, protect: 1, mirror: 1},
-		onBasePower(basePower, source, target, move) {
-			const item = target.getItem();
-			if (!this.singleEvent('TakeItem', item, target.itemState, target, target, move, item)) return;
-			if (item.id) {
-				return this.chainModify(1.5);
-			}
-		},
 		onAfterHit(target, source) {
 			if (source.hp) {
 				const item = target.takeItem();
@@ -29429,7 +29422,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		pp: 10,
 		priority: 0,
 		flags: {snatch: 1, heal: 1},
-		slotCondition: 'Wish',
+		slotCondition: 'Wish 2',
 		condition: {
 			duration: 2,
 			onStart(pokemon, source) {
