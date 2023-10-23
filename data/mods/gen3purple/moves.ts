b3958592faf73a207c9fb6b54d152b9f8d1cacfd
level2man <143778622+level2man@users.noreply.github.com>
2023-10-22 23:47:37 -0400

    Add files via upload

--- data/mods/gen3purple/moves.ts
+++ data/mods/gen3purple/moves.ts
@@ -7,4 +7,20 @@ export const Moves: {[k: string]: ModdedMoveData} = {
 		inherit: true,
 		gen: 3,
 	},
+	wish2: {
+		inherit: true,
+		flags: {heal: 1},
+		slotCondition: 'Wish 2',
+		condition: {
+			duration: 2,
+			onResidualOrder: 7,
+			onEnd(target) {
+				if (!target.fainted) {
+					const source = this.effectState.source;
+					const damage = this.heal(target.baseMaxhp / 2, target, target);
+					if (damage) this.add('-heal', target, target.getHealth, '[from] move: Wish', '[wisher] ' + source.name);
+				}
+			},
+		},
+	},
 };
