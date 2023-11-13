d2256d2e31f8da21c4d66ccecea6df1a92b88d62
level2man <143778622+level2man@users.noreply.github.com>
2023-11-12 20:58:28 -0500

    Update scripts.ts

--- data/mods/gen3purple/scripts.ts
+++ data/mods/gen3purple/scripts.ts
@@ -1,4 +1,16 @@
 export const Scripts: ModdedBattleScriptsData = {
 	gen: 3,
 	inherit: 'gen3',
+	
+        init() {
+                const originalMoves = this.mod().data.Moves;
+                let originalCategory = '';
+                for (const i in this.data.Moves) {
+                        if (this.data.Moves[i].category === 'Status') continue;
+                        originalCategory = originalMoves[i].category;
+                        if (originalCategory !== this.data.Moves[i].category) {
+                                this.modData('Moves', i).category = originalCategory;
+                        }
+                }
+        },
 };
