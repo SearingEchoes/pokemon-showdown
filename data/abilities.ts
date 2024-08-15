1fc171c871c71affb5f40aa5b7ab65b4625afbf7
level2man <143778622+level2man@users.noreply.github.com>
2024-08-15 16:46:05 -0400

    Add files via upload

--- data/abilities.ts
+++ data/abilities.ts
@@ -5920,7 +5920,7 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 	},
 	gunteleport: {
 		onStart(pokemon) {
-		if (this.turn > 0)
+		if (this.turn > 1)
 			this.add('-ability', pokemon, 'Gun Teleport');
 			this.actions.useMove('Samurai Edge', pokemon);
 		},
