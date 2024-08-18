91e40c962ead177aba62d8feabae535fca69e2ff
level2man <143778622+level2man@users.noreply.github.com>
2024-08-18 17:44:50 -0400

    Update scripts.ts

--- data/mods/gen3chaoticrift/scripts.ts
+++ data/mods/gen3chaoticrift/scripts.ts
@@ -78,58 +78,7 @@ export const Scripts: ModdedBattleScriptsData = {
 				}
 			},
 
-			runMove(moveOrMoveName, pokemon, targetLoc, sourceEffect, zMove, externalMove, maxMove, originalTarget) {
-				pokemon.activeMoveActions++;
-				let target = this.battle.getTarget(pokemon, maxMove || zMove || moveOrMoveName, targetLoc, originalTarget);
-				let baseMove = this.dex.getActiveMove(moveOrMoveName);
-				const pranksterBoosted = baseMove.pranksterBoosted;
-				if (baseMove.id !== 'struggle' && !zMove && !maxMove && !externalMove) {
-					const changedMove = this.battle.runEvent('OverrideAction', pokemon, target, baseMove);
-					if (changedMove && changedMove !== true) {
-						baseMove = this.dex.getActiveMove(changedMove);
-						if (pranksterBoosted) baseMove.pranksterBoosted = pranksterBoosted;
-						target = this.battle.getRandomTarget(pokemon, baseMove);
-					}
-				}
-				let move = baseMove;
-				if (zMove) {
-					move = this.getActiveZMove(baseMove, pokemon);
-				} else if (maxMove) {
-					move = this.getActiveMaxMove(baseMove, pokemon);
-				}
-
-				move.isExternal = externalMove;
-
-				this.battle.setActiveMove(move, pokemon, target);
-
-				/* if (pokemon.moveThisTurn) {
-					// THIS IS PURELY A SANITY CHECK
-					// DO NOT TAKE ADVANTAGE OF THIS TO PREVENT A POKEMON FROM MOVING;
-					// USE this.battle.queue.cancelMove INSTEAD
-					this.battle.debug('' + pokemon.id + ' INCONSISTENT STATE, ALREADY MOVED: ' + pokemon.moveThisTurn);
-					this.battle.clearActiveMove(true);
-					return;
-				} */
-				const willTryMove = this.battle.runEvent('BeforeMove', pokemon, target, move);
-				if (!willTryMove) {
-					this.battle.runEvent('MoveAborted', pokemon, target, move);
-					this.battle.clearActiveMove(true);
-					// The event 'BeforeMove' could have returned false or null
-					// false indicates that this counts as a move failing for the purpose of calculating Stomping Tantrum's base power
-					// null indicates the opposite, as the Pokemon didn't have an option to choose anything
-					pokemon.moveThisTurnResult = willTryMove;
-					return;
-				}
-				if (move.beforeMoveCallback) {
-					if (move.beforeMoveCallback.call(this.battle, pokemon, target, move)) {
-						this.battle.clearActiveMove(true);
-						pokemon.moveThisTurnResult = false;
-						return;
-					}
-				}
-				pokemon.lastDamage = 0;
-
-			},
+			
 
 
 
