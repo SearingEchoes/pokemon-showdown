d05fe566e54b6db222a7831aa762a25246ae9afc
level2man <143778622+level2man@users.noreply.github.com>
2024-08-18 22:30:15 -0400

    Add files via upload

--- data/mods/gen3chaoticrift/scripts.ts
+++ data/mods/gen3chaoticrift/scripts.ts
@@ -22,63 +22,93 @@ export const Scripts: ModdedBattleScriptsData = {
 	
 	actions: {
 
-			// 1 Z per pokemon
-			canZMove(pokemon) {
-				if (pokemon.m.zMoveUsed ||
-					(pokemon.transformed &&
-						(pokemon.species.isMega || pokemon.species.isPrimal || pokemon.species.forme === "Ultra"))
-				) return;
-				const item = pokemon.getItem();
-				if (!item.zMove) return;
-				if (item.itemUser && !item.itemUser.includes(pokemon.species.name)) return;
-				let atLeastOne = false;
-				let mustStruggle = true;
-				const zMoves: ZMoveOptions = [];
-				for (const moveSlot of pokemon.moveSlots) {
-					if (moveSlot.pp <= 0) {
-						zMoves.push(null);
-						continue;
-					}
-					if (!moveSlot.disabled) {
-						mustStruggle = false;
-					}
-					const move = this.dex.moves.get(moveSlot.move);
-					let zMoveName = this.getZMove(move, pokemon, true) || '';
-					if (zMoveName) {
-						const zMove = this.dex.moves.get(zMoveName);
-						if (!zMove.isZ && zMove.category === 'Status') zMoveName = "Z-" + zMoveName;
-						zMoves.push({move: zMoveName, target: zMove.target});
-					} else {
-						zMoves.push(null);
-					}
-					if (zMoveName) atLeastOne = true;
-				}
-				if (atLeastOne && !mustStruggle) return zMoves;
-			},
+	// 1 Z per pokemon
+	canZMove(pokemon) {
+		if (pokemon.m.zMoveUsed ||
+			(pokemon.transformed &&
+				(pokemon.species.isMega || pokemon.species.isPrimal || pokemon.species.forme === "Ultra"))
+		) return;
+		const item = pokemon.getItem();
+		if (!item.zMove) return;
+		if (item.itemUser && !item.itemUser.includes(pokemon.species.name)) return;
+		let atLeastOne = false;
+		let mustStruggle = true;
+		const zMoves: ZMoveOptions = [];
+		for (const moveSlot of pokemon.moveSlots) {
+			if (moveSlot.pp <= 0) {
+				zMoves.push(null);
+				continue;
+			}
+			if (!moveSlot.disabled) {
+				mustStruggle = false;
+			}
+			const move = this.dex.moves.get(moveSlot.move);
+			let zMoveName = this.getZMove(move, pokemon, true) || '';
+			if (zMoveName) {
+				const zMove = this.dex.moves.get(zMoveName);
+				if (!zMove.isZ && zMove.category === 'Status') zMoveName = "Z-" + zMoveName;
+				zMoves.push({move: zMoveName, target: zMove.target});
+			} else {
+				zMoves.push(null);
+			}
+			if (zMoveName) atLeastOne = true;
+		}
+		if (atLeastOne && !mustStruggle) return zMoves;
+	},
 
-			getZMove(move, pokemon, skipChecks) {
-				const item = pokemon.getItem();
-				if (!skipChecks) {
-					if (pokemon.m.zMoveUsed) return;
-					if (!item.zMove) return;
-					if (item.itemUser && !item.itemUser.includes(pokemon.species.name)) return;
-					const moveData = pokemon.getMoveData(move);
-					// Draining the PP of the base move prevents the corresponding Z-move from being used.
-					if (!moveData?.pp) return;
-				}
+	getZMove(move, pokemon, skipChecks) {
+		const item = pokemon.getItem();
+		if (!skipChecks) {
+			if (pokemon.m.zMoveUsed) return;
+			if (!item.zMove) return;
+			if (item.itemUser && !item.itemUser.includes(pokemon.species.name)) return;
+			const moveData = pokemon.getMoveData(move);
+			// Draining the PP of the base move prevents the corresponding Z-move from being used.
+			if (!moveData?.pp) return;
+		}
 
-				if (move.name === item.zMoveFrom) {
-					return item.zMove as string;
-				} else if (item.zMoveFrom === 'tbblade' && move.flags['slicing']) {
-					return item.zMove as string;
-				} else if (item.zMove === true && move.type === item.zMoveType) {
-					if (move.category === "Status") {
-						return move.name;
-					} else if (move.zMove?.basePower) {
-						return this.Z_MOVES[move.type];
-					}
-				}
-			},
+		if (move.name === item.zMoveFrom) {
+			return item.zMove as string;
+		} else if (item.zMoveFrom === 'tbblade' && move.flags['slicing']) {
+			return item.zMove as string;
+		} else if (item.zMove === true && move.type === item.zMoveType) {
+			if (move.category === "Status") {
+				return move.name;
+			} else if (move.zMove?.basePower) {
+				return this.Z_MOVES[move.type];
+			}
+		}
+	},
+
+	getActiveZMove(move: Move, pokemon: Pokemon): ActiveMove {
+		if (pokemon) {
+			const item = pokemon.getItem();
+			if (move.name === item.zMoveFrom) {
+				const zMove = this.dex.getActiveMove(item.zMove as string);
+				zMove.isZOrMaxPowered = true;
+				return zMove;
+			} else if (item.name == 'swordofzerker') {
+				const zMove = 'Thunderbolt Blade';
+				zMove.isZOrMaxPowered = true;
+				return zMove;	
+			}
+		}
+
+		if (move.category === 'Status') {
+			const zMove = this.dex.getActiveMove(move);
+			zMove.isZ = true;
+			zMove.isZOrMaxPowered = true;
+			return zMove;
+		}
+		
+		const zMove = this.dex.getActiveMove(this.Z_MOVES[move.type]);
+		zMove.basePower = move.zMove!.basePower!;
+		zMove.category = move.category;
+		// copy the priority for Quick Guard
+		zMove.priority = move.priority;
+		zMove.isZOrMaxPowered = true;
+		return zMove;
+	}
 			
 	runMove(
 		moveOrMoveName: Move | string, pokemon: Pokemon, targetLoc: number, sourceEffect?: Effect | null,
