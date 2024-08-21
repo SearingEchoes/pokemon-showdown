87ace8fc0ae05c1cd303d743a9f3c28cc289fa49
level2man <143778622+level2man@users.noreply.github.com>
2024-08-21 18:50:24 -0400

    Add files via upload

--- data/items.ts
+++ data/items.ts
@@ -8330,9 +8330,9 @@ export const Items: {[itemid: string]: ItemData} = {
 				for (const moveSlot of source.moveSlots) {
 					const activeMove = this.dex.getActiveMove(moveSlot.id);
 					this.singleEvent('DisableMove', activeMove, null, source);
-					this.debug(source.lastmove.id);
+					this.debug(source.lastmove);
 					if (activeMove.flags['cantusetwice'] && source.lastMove?.id === moveSlot.id) {
-						pokemon.disableMove(source.lastMove.id);
+						pokemon.disableMove(source.lastMove);
 					}
 				}
 		},
