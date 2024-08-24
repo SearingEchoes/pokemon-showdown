0c6a95985e44f7fc4e21ba98dc82a84dcb2589a7
level2man <143778622+level2man@users.noreply.github.com>
2024-08-24 15:23:05 -0400

    Add files via upload

--- data/moves.ts
+++ data/moves.ts
@@ -17624,7 +17624,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		priority: 0,
 		flags: {protect: 1, reflectable: 1, mirror: 1, allyanim: 1},
 		onHit(target) {
-			if (target.getTypes().join() === 'Water' || !target.setType('Water') || target.item === 'Sword of Zerker' || target.item === 'Rock of Saurian' || target.item === 'Star of Ninja') {
+			if (target.getTypes().join() === 'Water' || !target.setType('Water') || target.item.name === 'Sword of Zerker' || target.item.name === 'Rock of Saurian' || target.item.name === 'Star of Ninja') {
 				// Soak should animate even when it fails.
 				// Returning false would suppress the animation.
 				this.add('-fail', target);
