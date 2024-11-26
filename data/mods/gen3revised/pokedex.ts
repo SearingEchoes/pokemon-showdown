d49dd8e337f1d00efe872b9071c2396706f487b9
level2man <143778622+level2man@users.noreply.github.com>
2024-11-26 01:13:28 -0500

    Add files via upload

---
+++ data/mods/gen3revised/pokedex.ts
@@ -0,0 +1,13 @@
+export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+	chibireimu: {
+		inherit: true,
+		types: ["Normal", "Flying"],
+		baseStats: {hp: 50, atk: 50, def: 50, spa: 50, spd: 50, spe: 50},
+	},
+	reimu: {
+		inherit: true,
+		types: ["Faith", "Flying"],
+		baseStats: {hp: 60, atk: 75, def: 60, spa: 75, spd: 60, spe: 70},
+	},
+
+};
