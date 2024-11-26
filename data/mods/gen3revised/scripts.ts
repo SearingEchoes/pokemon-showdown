a4b004845c1683bac552124fea6234e89ffa4da3
level2man <143778622+level2man@users.noreply.github.com>
2024-11-26 01:41:37 -0500

    Add files via upload

--- data/mods/gen3revised/scripts.ts
+++ data/mods/gen3revised/scripts.ts
@@ -2,23 +2,7 @@ export const Scripts: ModdedBattleScriptsData = {
 	gen: 3,
 	inherit: 'gen3',
 	init() {
-		const specialTypes = ['Fire', 'Water', 'Grass', 'Ice', 'Electric', 'Dark', 'Psychic', 'Dragon'];
-		let newCategory = '';
-		for (const i in this.data.Moves) {
-			if (!this.data.Moves[i]) console.log(i);
-			if (
-				this.data.Moves[i].category === 'Status' || 
-				this.data.Moves[i].category === 'Physical' || 
-				this.data.Moves[i].category === 'Special' 
-				) continue; 
-			newCategory = specialTypes.includes(this.data.Moves[i].type) ? 'Special' : 'Physical';
-			if (newCategory !== this.data.Moves[i].category) {
-				this.modData('Moves', i).category = newCategory;
-			}
-		}
 	
-
-		
 		this.modData("Learnsets", "chibireimu").learnset.pound = ["3L1"];
 		this.modData("Learnsets", "chibireimu").learnset.gust = ["3L5"];
 		this.modData("Learnsets", "chibireimu").learnset.focusenergy = ["3L7"];
@@ -12139,56 +12123,56 @@ export const Scripts: ModdedBattleScriptsData = {
 		this.modData("Learnsets", "chibikasen").learnset.rockpolish = ["3T"];
 		this.modData("Learnsets", "chibikasen").learnset.extremespeed = ["3T"];
 		this.modData("Learnsets", "chibikasen").learnset.focuspunch = ["3T"];
+		this.modData("Learnsets", "tojiko").learnset.aeroblast = ["3T"];
+		this.modData("Learnsets", "tojiko").learnset.agility = ["3T"];
 		this.modData("Learnsets", "tojiko").learnset.astonish = ["3L1"];
-		this.modData("Learnsets", "tojiko").learnset.leer = ["3L5"];
-		this.modData("Learnsets", "tojiko").learnset.thundershock = ["3L7"];
-		this.modData("Learnsets", "tojiko").learnset.confuseray = ["3L10"];
-		this.modData("Learnsets", "tojiko").learnset.shockwave = ["3L14", "3M"];
-		this.modData("Learnsets", "tojiko").learnset.hex = ["3L17"];
-		this.modData("Learnsets", "tojiko").learnset.thunderwave = ["3L23", "3T"];
-		this.modData("Learnsets", "tojiko").learnset.whirlwind = ["3L27"];
-		this.modData("Learnsets", "tojiko").learnset.manacharge = ["3L30"];
-		this.modData("Learnsets", "tojiko").learnset.shadowball = ["3L34", "3M"];
-		this.modData("Learnsets", "tojiko").learnset.voltswitch = ["3L38"];
-		this.modData("Learnsets", "tojiko").learnset.memento = ["3L42"];
-		this.modData("Learnsets", "tojiko").learnset.uturn = ["3M"];
 		this.modData("Learnsets", "tojiko").learnset.calmmind = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.roar = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.toxic = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.hiddenpower = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.taunt = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.lunatic = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.lightscreen = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.protect = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.safeguard = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.razorwind = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.thunderbolt = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.thunder = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.return = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.manaburst = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.reflect = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.signalbeam = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.facade = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.rest = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.thief = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.mysticfire = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.flash = ["3M"];
-		this.modData("Learnsets", "tojiko").learnset.shadowdive = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.confuseray = ["3L10"];
 		this.modData("Learnsets", "tojiko").learnset.curse = ["3T"];
-		this.modData("Learnsets", "tojiko").learnset.payback = ["3T"];
-		this.modData("Learnsets", "tojiko").learnset.sleeptalk = ["3T"];
+		this.modData("Learnsets", "tojiko").learnset.darkpulse = ["3T"];
+		this.modData("Learnsets", "tojiko").learnset.dreameater = ["3T"];
+		this.modData("Learnsets", "tojiko").learnset.facade = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.flash = ["3M"];
 		this.modData("Learnsets", "tojiko").learnset.haze = ["3T"];
-		this.modData("Learnsets", "tojiko").learnset.swagger = ["3T"];
+		this.modData("Learnsets", "tojiko").learnset.hex = ["3L17"];
+		this.modData("Learnsets", "tojiko").learnset.hiddenpower = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.leer = ["3L5"];
+		this.modData("Learnsets", "tojiko").learnset.lightscreen = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.lunatic = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.manaburst = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.manacharge = ["3L30"];
+		this.modData("Learnsets", "tojiko").learnset.memento = ["3L42"];
 		this.modData("Learnsets", "tojiko").learnset.metronome = ["3T"];
 		this.modData("Learnsets", "tojiko").learnset.mimic = ["3T"];
-		this.modData("Learnsets", "tojiko").learnset.dreameater = ["3T"];
-		this.modData("Learnsets", "tojiko").learnset.willowisp = ["3T"];
-		this.modData("Learnsets", "tojiko").learnset.agility = ["3T"];
-		this.modData("Learnsets", "tojiko").learnset.snatch = ["3T"];
-		this.modData("Learnsets", "tojiko").learnset.darkpulse = ["3T"];
-		this.modData("Learnsets", "tojiko").learnset.tailwind = ["3T"];
+		this.modData("Learnsets", "tojiko").learnset.mysticfire = ["3M"];
 		this.modData("Learnsets", "tojiko").learnset.nightshade = ["3T"];
-		this.modData("Learnsets", "tojiko").learnset.aeroblast = ["3T"];
+		this.modData("Learnsets", "tojiko").learnset.payback = ["3T"];
+		this.modData("Learnsets", "tojiko").learnset.protect = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.razorwind = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.reflect = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.rest = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.return = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.roar = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.safeguard = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.shadowball = ["3L34", "3M"];
+		this.modData("Learnsets", "tojiko").learnset.shadowdive = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.shockwave = ["3L14", "3M"];
+		this.modData("Learnsets", "tojiko").learnset.signalbeam = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.sleeptalk = ["3T"];
+		this.modData("Learnsets", "tojiko").learnset.snatch = ["3T"];
+		this.modData("Learnsets", "tojiko").learnset.swagger = ["3T"];
+		this.modData("Learnsets", "tojiko").learnset.tailwind = ["3T"];
+		this.modData("Learnsets", "tojiko").learnset.taunt = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.thief = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.thunder = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.thunderbolt = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.thundershock = ["3L7"];
+		this.modData("Learnsets", "tojiko").learnset.thunderwave = ["3L23", "3T"];
+		this.modData("Learnsets", "tojiko").learnset.toxic = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.uturn = ["3M"];
+		this.modData("Learnsets", "tojiko").learnset.voltswitch = ["3L38"];
+		this.modData("Learnsets", "tojiko").learnset.whirlwind = ["3L27"];
+		this.modData("Learnsets", "tojiko").learnset.willowisp = ["3T"];
 
 		this.modData("Learnsets", "tori").learnset.sandattack = ["3L1"];
 		this.modData("Learnsets", "tori").learnset.peck = ["3L1"];
@@ -13087,224 +13071,5 @@ export const Scripts: ModdedBattleScriptsData = {
 		this.modData("Learnsets", "elis").learnset.dreameater = ["3T"];
 	},
 	
-	actions: {
-		inherit: true,
-		getDamage(source, target, move, suppressMessages) {
-			if (typeof move === 'string') move = this.dex.getActiveMove(move);
 
-			if (typeof move === 'number') {
-				const basePower = move;
-				move = new Dex.Move({
-					basePower,
-					type: '???',
-					category: 'Physical',
-					willCrit: false,
-				}) as ActiveMove;
-				move.hit = 0;
-			}
-
-			if (!move.ignoreImmunity || (move.ignoreImmunity !== true && !move.ignoreImmunity[move.type])) {
-				if (!target.runImmunity(move.type, !suppressMessages)) {
-					return false;
-				}
-			}
-
-			if (move.ohko) return target.maxhp;
-			if (move.damageCallback) return move.damageCallback.call(this.battle, source, target);
-			if (move.damage === 'level') {
-				return source.level;
-			} else if (move.damage) {
-				return move.damage;
-			}
-
-			const category = this.battle.getCategory(move);
-
-			let basePower: number | false | null = move.basePower;
-			if (move.basePowerCallback) {
-				basePower = move.basePowerCallback.call(this.battle, source, target, move);
-			}
-			if (!basePower) return basePower === 0 ? undefined : basePower;
-			basePower = this.battle.clampIntRange(basePower, 1);
-
-			let critMult;
-			let critRatio = this.battle.runEvent('ModifyCritRatio', source, target, move, move.critRatio || 0);
-			if (this.battle.gen <= 5) {
-				critRatio = this.battle.clampIntRange(critRatio, 0, 5);
-				critMult = [0, 16, 8, 2, 1];
-			} else {
-				critRatio = this.battle.clampIntRange(critRatio, 0, 4);
-				if (this.battle.gen === 6) {
-					critMult = [0, 16, 8, 2, 1];
-				} else {
-					critMult = [0, 16, 8, 2, 1];
-				}
-			}
-
-			const moveHit = target.getMoveHitData(move);
-			moveHit.crit = move.willCrit || false;
-			if (move.willCrit === undefined) {
-				if (critRatio) {
-					moveHit.crit = this.battle.randomChance(1, critMult[critRatio]);
-				}
-			}
-
-			if (moveHit.crit) {
-				moveHit.crit = this.battle.runEvent('CriticalHit', target, null, move);
-			}
-
-			// happens after crit calculation
-			basePower = this.battle.runEvent('BasePower', source, target, move, basePower, true);
-
-			if (!basePower) return 0;
-			basePower = this.battle.clampIntRange(basePower, 1);
-			// Hacked Max Moves have 0 base power, even if you Dynamax
-			if ((!source.volatiles['dynamax'] && move.isMax) || (move.isMax && this.dex.moves.get(move.baseMove).isMax)) {
-				basePower = 0;
-			}
-
-			if (
-				basePower < 60 && source.getTypes(true).includes(move.type) && source.terastallized && move.priority <= 0 &&
-				// Hard move.basePower check for moves like Dragon Energy that have variable BP
-				!move.multihit && !((move.basePower === 0 || move.basePower === 150) && move.basePowerCallback)
-			) {
-				basePower = 60;
-			}
-
-			const level = source.level;
-
-			const attacker = move.overrideOffensivePokemon === 'target' ? target : source;
-			const defender = move.overrideDefensivePokemon === 'source' ? source : target;
-
-			const isPhysical = move.category === 'Physical';
-			let attackStat: StatIDExceptHP = move.overrideOffensiveStat || (isPhysical ? 'atk' : 'spa');
-			const defenseStat: StatIDExceptHP = move.overrideDefensiveStat || (isPhysical ? 'def' : 'spd');
-
-			const statTable = {atk: 'Atk', def: 'Def', spa: 'SpA', spd: 'SpD', spe: 'Spe'};
-
-			let atkBoosts = attacker.boosts[attackStat];
-			let defBoosts = defender.boosts[defenseStat];
-
-			let ignoreNegativeOffensive = !!move.ignoreNegativeOffensive;
-			let ignorePositiveDefensive = !!move.ignorePositiveDefensive;
-
-			if (moveHit.crit) {
-				ignoreNegativeOffensive = true;
-				ignorePositiveDefensive = true;
-			}
-			const ignoreOffensive = !!(move.ignoreOffensive || (ignoreNegativeOffensive && atkBoosts < 0));
-			const ignoreDefensive = !!(move.ignoreDefensive || (ignorePositiveDefensive && defBoosts > 0));
-
-			if (ignoreOffensive) {
-				this.battle.debug('Negating (sp)atk boost/penalty.');
-				atkBoosts = 0;
-			}
-			if (ignoreDefensive) {
-				this.battle.debug('Negating (sp)def boost/penalty.');
-				defBoosts = 0;
-			}
-
-			let attack = attacker.calculateStat(attackStat, atkBoosts, 1, source);
-			let defense = defender.calculateStat(defenseStat, defBoosts, 1, target);
-
-			attackStat = (category === 'Physical' ? 'atk' : 'spa');
-
-			// Apply Stat Modifiers
-			attack = this.battle.runEvent('Modify' + statTable[attackStat], source, target, move, attack);
-			defense = this.battle.runEvent('Modify' + statTable[defenseStat], target, source, move, defense);
-
-			if (this.battle.gen <= 4 && ['explosion', 'selfdestruct', 'explosion2', 'selfdestruct2', 'explode'].includes(move.id) && defenseStat === 'def') {
-				defense = this.battle.clampIntRange(Math.floor(defense / 2), 1);
-			}
-
-			const tr = this.battle.trunc;
-
-			// int(int(int(2 * L / 5 + 2) * A * P / D) / 50);
-			const baseDamage = tr(tr(tr(tr(2 * level / 5 + 2) * basePower * attack) / defense) / 50);
-
-			// Calculate damage modifiers separately (order differs between generations)
-			return this.modifyDamage(baseDamage, source, target, move, suppressMessages);
-		},
-		modifyDamage(baseDamage, pokemon, target, move, suppressMessages = false) {
-			// RSE divides modifiers into several mathematically important stages
-			// The modifiers run earlier than other generations are called with ModifyDamagePhase1 and ModifyDamagePhase2
-
-			if (!move.type) move.type = '???';
-			const type = move.type;
-
-			// Burn
-			if (pokemon.status === 'brn' && baseDamage && move.category === 'Physical' && !pokemon.hasAbility('guts')) {
-				baseDamage = this.battle.modify(baseDamage, 0.5);
-			}
-
-			// Other modifiers (Reflect/Light Screen/etc)
-			baseDamage = this.battle.runEvent('ModifyDamagePhase1', pokemon, target, move, baseDamage);
-
-			// Double battle multi-hit
-			// In Generation 3, the spread move modifier is 0.5x instead of 0.75x. Moves that hit both foes
-			// and the user's ally, like Earthquake and Explosion, don't get affected by spread modifiers
-			if (move.spreadHit && move.target === 'allAdjacentFoes') {
-				const spreadModifier = move.spreadModifier || 0.5;
-				this.battle.debug('Spread modifier: ' + spreadModifier);
-				baseDamage = this.battle.modify(baseDamage, spreadModifier);
-			}
-
-			// Weather
-			baseDamage = this.battle.runEvent('WeatherModifyDamage', pokemon, target, move, baseDamage);
-
-			if (move.category === 'Physical' && !Math.floor(baseDamage)) {
-				baseDamage = 1;
-			}
-
-			baseDamage += 2;
-
-			const isCrit = target.getMoveHitData(move).crit;
-			if (isCrit) {
-				baseDamage = this.battle.modify(baseDamage, move.critModifier || 1.5);
-			}
-
-			// Mod 2 (Damage is floored after all multipliers are in)
-			baseDamage = Math.floor(this.battle.runEvent('ModifyDamagePhase2', pokemon, target, move, baseDamage));
-
-			// STAB
-			if (move.forceSTAB || type !== '???' && pokemon.hasType(type)) {
-				// The "???" type never gets STAB
-				// Not even if you Roost in Gen 4 and somehow manage to use
-				// Struggle in the same turn.
-				// (On second thought, it might be easier to get a MissingNo.)
-				baseDamage = this.battle.modify(baseDamage, move.stab || 1.5);
-			}
-			// types
-			let typeMod = target.runEffectiveness(move);
-			typeMod = this.battle.clampIntRange(typeMod, -6, 6);
-			target.getMoveHitData(move).typeMod = typeMod;
-			if (typeMod > 0) {
-				if (!suppressMessages) this.battle.add('-supereffective', target);
-
-				for (let i = 0; i < typeMod; i++) {
-					baseDamage *= 2;
-				}
-			}
-			if (typeMod < 0) {
-				if (!suppressMessages) this.battle.add('-resisted', target);
-
-				for (let i = 0; i > typeMod; i--) {
-					baseDamage = Math.floor(baseDamage / 2);
-				}
-			}
-
-			if (isCrit && !suppressMessages) this.battle.add('-crit', target);
-
-			// Final modifier.
-			baseDamage = this.battle.runEvent('ModifyDamage', pokemon, target, move, baseDamage);
-
-			// this is not a modifier
-			baseDamage = this.battle.randomizer(baseDamage);
-
-			if (!Math.floor(baseDamage)) {
-				return 1;
-			}
-
-			return Math.floor(baseDamage);
-		},
-	},
 };
