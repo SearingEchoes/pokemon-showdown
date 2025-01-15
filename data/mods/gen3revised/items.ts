2884a77a1edcd77b0e54a6b1ecb4d76590a7fa5e
level2man <143778622+level2man@users.noreply.github.com>
2025-01-15 14:01:58 -0500

    Update items.ts

--- data/mods/gen3revised/items.ts
+++ data/mods/gen3revised/items.ts
@@ -272,7 +272,7 @@ export const Items: {[k: string]: ModdedItemData} = {
 	psncureberry: null,
 	doublesoul: {
 		inherit: true,
-		isNonstandard: 'Unobtainable',
+		isNonstandard: "Unobtainable",
 	},
 	//bigcloud: null,
 	//darkribbon: null,
