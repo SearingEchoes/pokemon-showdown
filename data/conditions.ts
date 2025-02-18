71e6b38642f0e28c2393079a4ac7a874a7cde40c
level2man <143778622+level2man@users.noreply.github.com>
2025-02-17 23:59:06 -0500

    Add files via upload

--- data/conditions.ts
+++ data/conditions.ts
@@ -916,7 +916,8 @@ export const Conditions: {[k: string]: ConditionData} = {
 		name: 'lifeaura',
 		duration: 4,
 		onTryHit(target, source, move) {
-			
+		
+			this.debug(move.id);
 			this.debug(move.basepower);
 		
 			if (move.basepower < 100 || move.category === 'Status' ) {
