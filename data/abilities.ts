94f6e40c4719245d14875d101f199e30463d3630
level2man <143778622+level2man@users.noreply.github.com>
2026-01-19 10:46:48 -0700

    Update abilities.ts

--- data/abilities.ts
+++ data/abilities.ts
@@ -6094,7 +6094,7 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 				return;
 			}
 
-			const armorTailHolder = this.effectState.target;
+			const dazzlingHolder = this.effectState.target;
 			if ((source.isAlly(dazzlingHolder) || move.target === 'all') && move.priority > 0.1) {
 				this.attrLastMove('[still]');
 				this.add('cant', dazzlingHolder, 'ability: Interdict', move, '[of] ' + target);
